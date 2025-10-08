6d50d2dc331ab15ad39841aaa1bc3e606486929f
mr-mashanlo <mr.mashanlo@yandex.ru>
2025-10-08 20:53:09 +0500

    refactor sign up entity and page

---
+++ src/features/signup-form/model/hook.tsx
@@ -0,0 +1,34 @@
+import { useForm } from '@tanstack/react-form';
+import { HTTPError } from 'ky';
+
+import { useSession } from '@/entities/session';
+import { mapServerErrors } from '@/shared/mappers';
+
+interface Props {
+  onSuccess?: () => void,
+  onError?: () => void
+}
+
+const useSignupForm = ( { onSuccess, onError }: Props = {} ) => {
+  const { signup } = useSession();
+
+  const form = useForm( {
+    defaultValues: { email: '', password: '' },
+    onSubmit: async ( { value, formApi } ) => {
+      try {
+        await signup( value );
+        onSuccess?.();
+      } catch ( error ) {
+        if ( error instanceof HTTPError ) {
+          const errors = await error.response.json();
+          formApi.setErrorMap( { onChange: { fields: mapServerErrors( errors.issues ) } } );
+        }
+        onError?.();
+      }
+    }
+  } );
+
+  return form;
+};
+
+export default useSignupForm;
--- src/pages/sign-up/ui/page.tsx
+++ src/pages/sign-up/ui/page.tsx
@@ -1,9 +1,21 @@
-import { type FC } from 'react';
+import { type FC, useState } from 'react';
 
 import { SignUpForm } from '@/features/signup-form';
+import { Family } from '@/widgets/family';
 
 const SignupPage: FC = () => {
-  return <SignUpForm />;
+  const [ mood, setMood ] = useState<'neutral' | 'angry' | 'sad'>( 'neutral' );
+
+  return (
+    <section className="w-full grid sm:grid-cols-2 gap-10 sm:gap-4">
+      <div className="flex justify-center items-center">
+        <Family mood={mood} />
+      </div>
+      <div className="flex justify-center items-center">
+        <SignUpForm setMood={setMood} />
+      </div>
+    </section>
+  );
 };
 
 export default SignupPage;
