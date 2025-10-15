665b7ff5dbbbd8ebe39214e79365f6692508e3d9
mr-mashanlo <mr.mashanlo@yandex.ru>
2025-10-15 21:47:57 +0500

    update ky instance

--- src/shared/libs/ky.ts
+++ src/shared/libs/ky.ts
@@ -1,12 +1,23 @@
 import ky from 'ky';
 
+import { useSessionStore } from '@/entities/session';
+
 export const kyInstance = ky.create( {
   prefixUrl: 'http://localhost:4173',
   credentials: 'include',
   headers: { 'Content-Type': 'application/json' },
   retry: {
     limit: 1,
-    statusCodes: [ 400, 401 ],
+    statusCodes: [ 400 ],
     methods: [ 'get', 'post', 'put', 'delete' ]
+  },
+  hooks: {
+    afterResponse: [
+      async ( _request, _options, response ) => {
+        if ( response.status === 401 ) {
+          useSessionStore.getState().setIsAuthorized( false );
+        }
+      }
+    ]
   }
 } );
