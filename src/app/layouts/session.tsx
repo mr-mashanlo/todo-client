65ef95dd088bfbf6af706050498c4acf91faadb0
mr-mashanlo <mr.mashanlo@yandex.ru>
2025-10-29 15:23:25 +0500

    refactor styles

--- src/app/layouts/session.tsx
+++ src/app/layouts/session.tsx
@@ -2,11 +2,7 @@ import { type FC } from 'react';
 import { Outlet } from 'react-router';
 
 const SessionLayout: FC = () => {
-  return (
-    <main className="min-h-screen p-4 sm:p-5 flex items-center justify-center">
-      <Outlet />
-    </main>
-  );
+  return <Outlet />;
 };
 
 export default SessionLayout;
