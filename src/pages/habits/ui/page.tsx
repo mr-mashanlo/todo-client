8e3655919a507317c03ac8e0bf0b6de4276459eb
mr-mashanlo <mr.mashanlo@yandex.ru>
2025-10-30 12:17:16 +0500

    refactor habits page

--- src/pages/habits/ui/page.tsx
+++ src/pages/habits/ui/page.tsx
@@ -1,11 +1,18 @@
 import { type FC } from 'react';
 
+import { CreateHabitButton } from '@/features/create-habit-form';
+import { NoteIcon } from '@/shared/icons';
 import { Habits } from '@/widgets/habits';
 
 export const HabitsPage: FC = () => {
   return (
     <div className="p-4 sm:p-15">
-      <h2 className="mb-8 font-bold text-2xl">Habits</h2>
+      <div className="mb-8 flex items-center gap-4">
+        <h2 className="font-bold text-2xl">Habits</h2>
+        <CreateHabitButton className="cursor-pointer">
+          <NoteIcon className="w-6 h-6" />
+        </CreateHabitButton>
+      </div>
       <Habits />
     </div>
   );
