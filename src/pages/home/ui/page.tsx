e9d4ccf6d403a25a17b84b71e59f2c2be748a8d0
mr-mashanlo <mr.mashanlo@yandex.ru>
2025-11-05 16:05:03 +0500

    update home page

--- src/pages/home/ui/page.tsx
+++ src/pages/home/ui/page.tsx
@@ -1,9 +1,17 @@
 import { type FC } from 'react';
+import { Link } from 'react-router';
+
+import { Progress } from '@/widgets/progress';
 
 export const HomePage: FC = () => {
   return (
     <div className="p-4 sm:p-15">
-      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quis, similique.
+      <div className="mb-8 flex items-center gap-4">
+        <h2 className="font-bold text-2xl">Today</h2>
+        <span className="font-bold text-2xl text-zinc-200">/</span>
+        <Link to="/habits" className="font-bold text-2xl text-zinc-200 hover:underline hover:text-black">go to habits</Link>
+      </div>
+      <Progress />
     </div>
   );
 };
